597d69fcfa352804506a6085c0cc122d8708c4a1
jamesx <master@umaiki.com>
2016-06-21 23:46:46 +0000

    Fixed some client issues and connected client to backend

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -15,9 +15,13 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'restangular'
 ])
-.config(function ($routeProvider) {
+.config(function ($routeProvider, RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('http://54.183.97.63:3000');
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -29,7 +33,27 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
     })
+    .when('/products', {
+      templateUrl: 'views/products.html',
+      controller: 'ProductsCtrl',
+      controllerAs: 'products'
+    })
     .otherwise({
         redirectTo: '/'
     });
+})
+
+// Override the restangular framework's id keyword from id to _id,
+// as _id is used in nodejs
+.factory('ProductRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setRestangularFields({
+            id: '_id'
+        });
+    });
+})
+
+// Provide the product factory
+.factory('Product', function(ProductRestangular) {
+    return ProductRestangular.service('products');
 });
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'product': require('./controllers/product_controller')
+    'products': require('./controllers/product_controller')
 };
