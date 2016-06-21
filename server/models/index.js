fd62b2afaa3df7895a4c9cfcc11718ea49cf3a74
JamesX <jamesx@ip-172-31-4-128.us-west-1.compute.internal>
2016-06-21 21:33:45 +0000

    General cleanup, server tested working

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    product: require('./Product')
+    product: require('./product')
 };
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'product': require('./controllers/product_controllers')
+    'product': require('./controllers/product_controller')
 };
