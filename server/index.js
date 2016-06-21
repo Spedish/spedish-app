ea2ed98685dc8929569212dabd0f31584afb3401
Huaxi Wang <huaxi.wang@gmail.com>
2016-06-21 00:43:37 -0700

    Created simple app prototype with dependencies and a basic CRUD api on products.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
-var express  = require('express');
+var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
 
-var app      = express();
+// Create the application.
+var app = express();
 
-mongoose.connect('mongodb://localhost/server');
+// Add Middleware necessary for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/spedish');
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', function() {
-    console.log('Connection opened');
+
+    // Load the models.
+    app.models = require('./models/index');
+
+    // Load the routes.
+    var routes = require('./routes');
+    _.each(routes, function(controller, route) {
+      app.use(route, controller(app, route));
+    });
+
+    console.log('Listening on port 3000...');
     app.listen(3000);
 });
---
+++ server/models/Product.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+// Create the Product Schema.
+var ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  inventory: {
+    type: Number,
+    required: true
+  }
+});
+
+// Export the model.
+module.exports = mongoose.model('product', ProductSchema);
