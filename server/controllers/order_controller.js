390c3050f70e3eae71ece4b919ed4a58cf422a9b
Huaxi Wang <huaxi.wang@gmail.com>
2016-06-28 01:17:41 -0700

    Added order post api and place holder for order search.

--- server/controllers/order_controller.js
+++ server/controllers/order_controller.js
@@ -1,8 +1,38 @@
 var Resource = require('resourcejs');
+var Item = require('../models/item.js');
 
 module.exports = function(app, route) {
-  // Setup the controller for REST;
-  Resource(app, '', route, app.models.order).rest();
+  app.post('/order', function(req, res) {
+    Item.findById(req.body.item_id, function (err, item) {
+      if (err) return next(err);
+      if (item.inventory >= req.body.count) {
+        item.inventory = (item.inventory - req.body.count);
+        item.orders.push(req.body);
+        item.save(function(err, docs) {
+          if (err) return handleError(error)
+          console.log('Inventory successfully updated!');
+          res.json(docs);
+        });
+      } else res.json({
+            status: 'failure',
+            message: "We don't have enough inventory."
+        });
+    });
+  });
+
+  app.get('/order/:id', function(req, res) {
+    Order.findById(req.path.id, function (err, order) {
+      if (err) return next(err);
+      if (order) {
+        res.json(order);
+      } else res.json({
+            status: 'failure',
+            message: "We didn't find this order."
+        });
+    });
+  });
+
+
 
   // Return middleware.
   return function(req, res, next) {
--- server/models/item.js
+++ server/models/item.js
@@ -1,6 +1,49 @@
 var mongoose = require('mongoose');
 
-var ItemSchema = new mongoose.Schema({
+var orderSchema = new mongoose.Schema({
+    item_id: {
+      type: String,
+      required: true
+    },
+    count: {
+      type: Number,
+      min: [0],
+      required: true
+    },
+    total_price: {
+      type: Number,
+      min: [0],
+      required: true
+    },
+    updated_date: {
+      type: Date
+    },
+    order_date: {
+      type: Date
+    },
+    status: {
+      type: String,
+      enum: ['ordered', 'paid', 'ready for pickup', 'picked up'],
+      required: true
+    }
+  }
+);
+
+orderSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_date field to current date
+  this.updated_date = currentDate;
+
+  // if order_date doesn't exist, add to that field
+  if (!this.order_date)
+    this.order_date = currentDate;
+
+  next();
+});
+
+var itemSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true
@@ -11,13 +54,37 @@ var ItemSchema = new mongoose.Schema({
   },
   price: {
     type: Number,
+    min: [0],
     required: true
   },
   inventory: {
     type: Number,
     required: true
-  }
+  },
+  orders: [orderSchema],
+  updated_date: {
+    type: Date
+  },
+  create_date: {
+    type: Date
+  },
+});
+
+itemSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_date field to current date
+  this.updated_date = currentDate;
+
+  // if order_date doesn't exist, add to that field
+  if (!this.create_date)
+    this.create_date = currentDate;
+
+  next();
 });
 
 // Export the model.
-module.exports = mongoose.model('item', ItemSchema);
+var Item = mongoose.model('Item', itemSchema);
+var Order = mongoose.model('Order', orderSchema);
+module.exports = Item;
