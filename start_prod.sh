90a00230d811947c8994b315840f0280bc826e8a
jamesx <master@umaiki.com>
2016-07-10 17:49:46 +0000

    Completing dependency info and correcting issue with prod config

---
+++ start_prod.sh
@@ -0,0 +1,26 @@
+# Sync master from git
+git fetch origin master
+
+# Check if there is a prod mongodb already started
+PID=`ps -ef | grep mongod | grep '/opt/spedish/db' | grep -v grep | awk '{print $2}'`
+
+if [ ! -z "$PID" ]; then
+  echo Stopping mongod with PID $PID
+  kill -15 $PID
+fi
+
+# Start Mongodb
+nohup mongod --dbpath /opt/spedish/db &
+
+# Stop NodeJS server
+PID=`ps -ef | grep nodejs | grep '--prod' | grep -v grep | awk '{print $2}'`
+
+if [ ! -z "$PID" ]; then
+  echo Stopping nodejs with PID $PID
+  kill -15 $PID
+fi
+
+# Install server dependencies
+
+
+
