37f02dd665d1f7a7146003c8fb49c5a781dd3c32
jamesx <master@umaiki.com>
2016-07-11 02:31:15 +0000

    Updating start script

--- start_prod.sh
+++ start_prod.sh
@@ -1,44 +1,50 @@
-echo Sync to latest git master
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}Sync to latest git master${NC}"
 git fetch origin master
 
-echo Stopping mongodb
+echo -e "${RED}Stopping mongodb${NC}"
 PID=`ps -ef | grep mongod | grep '/opt/spedish/db' | grep -v grep | awk '{print $2}'`
 
 if [ ! -z "$PID" ]; then
-  echo Stopping mongod with PID $PID
+  echo -e "${RED}Stopping mongod with PID ${PID} ${NC}"
   kill -15 $PID
 fi
 
-echo Restarting mongodb
+echo -e "${RED}Restarting mongodb${NC}"
 nohup mongod --dbpath /opt/spedish/db &
 
-echo Stopping nodejs
-PID=`ps -ef | grep nodejs | grep '--prod' | grep -v grep | awk '{print $2}'`
+echo -e "${RED}Stopping nodejs${NC}"
+PID=`lsof -i :4000 | grep nodejs | awk '{print $2}'`
 
 if [ ! -z "$PID" ]; then
-  echo Stopping nodejs with PID $PID
+  echo -e "${RED}Stopping nodejs with PID $PID ${NC}"
   kill -15 $PID
 fi
 
-echo Installing server dependecies
+echo -e "${RED}Installing server dependecies${NC}"
 cd server
 npm install
 
-echo Starting nodejs
-nohup nodejs index.js --prod &
-cd ..
+echo -e "${RED}Starting nodejs${NC}"
+export NODE_ENV=prod
+nohup nodejs index.js &
 
-echo Stopping nginx
+echo -e "${RED}Stopping nginx${NC}"
 sudo service nginx stop
 
-echo Installing frontend dependencies
+echo -e "${RED}Installing frontend dependencies${NC}"
+cd ../client
 npm install
 bower install
 
-echo Building frontend dist package
+echo -e "${RED}Building frontend dist package${NC}"
 rvm use system
 grunt build
 
-echo Restarting nginx
+echo -e "${RED}Restarting nginx${NC}"
 sudo service nginx restart
 
+# Restore pwd
+cd ..
