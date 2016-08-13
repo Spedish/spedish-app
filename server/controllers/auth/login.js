1f210d2c7dac70d739b72130e83f0a48d0dff5f8
jamesx <master@umaiki.com>
2016-08-13 23:03:50 +0000

    Passing User.isSeller to frontend and UI update
    This fixes #28

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -3,7 +3,7 @@ module.exports = function(app, route, passport) {
   app.post('/login', function(req, res, next) {
     passport.authenticate('local-login', function(err, user) {
       req.logIn(user, function() {
-        res.status(200).send(user);
+        res.status(200).send({isSeller: user.isSeller});
       })
     })(req, res, next);
   });
