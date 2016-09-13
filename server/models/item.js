3d6215fd91b348e5a52ebfb9756e167208ff441c
jamesx <master@umaiki.com>
2016-09-13 07:27:56 +0000

    Added a set of pre/post item model hooks

--- server/models/item.js
+++ server/models/item.js
@@ -129,8 +129,33 @@ var autoPopulate = function(next) {
   next();
 }
 
+// apply complex logic filter
+var applyPreFilter = function(next) {
+  this._postConditions = {};
+
+  // Skip known keywords, since we will be doing post processing on it
+  if (this._conditions.abcd) {
+    this._postConditions['abcd'] = this._conditions.abcd;
+    delete this._conditions['abcd'];
+  }
+
+  next();
+}
+
+var applyPostFilter = function(results, next) {
+  if (this._postConditions.abcd)
+  {
+    console.log("perform post processing on this");
+  }
+
+  next();
+}
+
 itemSchema.pre('findOne', autoPopulate);
+itemSchema.pre('find', applyPreFilter);
 itemSchema.pre('find', autoPopulate);
+itemSchema.pre('count', applyPreFilter);
+itemSchema.post('find', applyPostFilter);
 
 // Export the model.
 var Item = mongoose.model('Item', itemSchema);
