7da93787dd377694d0d75a6a63a09566909ac7b6
XinchenShen <xinchen.shen@gmail.com>
2016-10-01 10:32:23 -0700

    Minor changes based on the new URL

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -13,53 +13,45 @@ module.exports = function(app, route, passport) {
     var orderId = req.params.orderId;
     var completeOrderId = req.params.completeOrderId;
 
-    if (orderId != undefined && completeOrderId != undefined) {
-      Order.findById(orderId, function(err, order) {
-        if (err) {
-          res.status(404).json({
-            status: 'failure',
-            message: "Order with orderId " + orderId + " can not be found."
-          });
+    Order.findById(orderId, function(err, order) {
+      if (err) {
+        res.status(404).json({
+          status: 'failure',
+          message: "Order with orderId " + orderId + " can not be found."
+        });
+      }
+      else {
+        if (order.status == 'complete') {
+          res.status(200).json("Order is already complete.");
         }
         else {
-          if (order.status == 'complete') {
-            res.status(200).json("Order is already complete.");
-          }
-          else {
-            if (order.complete_order_id == completeOrderId) {
-              if (order.status == 'ready') {
-                order.status = 'complete';
-                order.save(function(err, order) {
-                  if (err) res.status(500).json({
-                    status: 'failure',
-                    message: "Update order with orderId " + orderId + " failed."
-                  });
-                  res.status(200).json("Order is complete.");
-                });
-              }
-              else {
-                res.status(500).json({
+          if (order.complete_order_id == completeOrderId) {
+            if (order.status == 'ready') {
+              order.status = 'complete';
+              order.save(function(err, order) {
+                if (err) res.status(500).json({
                   status: 'failure',
-                  message: "Can not complete order with orderId " + orderId + ". Order is not under ready status."
+                  message: "Update order with orderId " + orderId + " failed."
                 });
-              }
+                res.status(200).json("Order is complete.");
+              });
             }
             else {
-              res.status(404).json({
+              res.status(500).json({
                 status: 'failure',
-                message: "Order with orderId " + orderId + " does not contain completeOrderId " + completeOrderId + " ."
+                message: "Can not complete order with orderId " + orderId + ". Order is not under ready status."
               });
             }
           }
+          else {
+            res.status(404).json({
+              status: 'failure',
+              message: "Order with orderId " + orderId + " does not contain completeOrderId " + completeOrderId + " ."
+            });
+          }
         }
-      });
-    }
-    else {
-      res.status(404).json({
-        status: 'failure',
-        message: "Wrong request formatt. Please set orderId and completeOrderId in URL."
-      });
-    }
+      }
+    });
   });
 
   Resource(app, '', route, app.models.order)
