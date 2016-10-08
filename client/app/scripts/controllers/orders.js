44691844ca7db3354bfbf1658a2e5975ccff62b0
11xi <xichen022@gmail.com>
2016-10-07 20:28:30 -0700

    Create order view

--- client/app/scripts/controllers/orders.js
+++ client/app/scripts/controllers/orders.js
@@ -1,5 +1,28 @@
 'use strict';
 
-angular.module('clientApp')
-  .controller('OrdersCtrl', function($scope, $routeParams, $location, Item) {
+angular.module('clientApp').controller('OrdersCtrl', function($scope, Order) {
+    // Pagination config
+    $scope.currentPage = 1;
+    $scope.limit = 4;
+    $scope.totalItems;
+
+    var requestParams = {
+        limit: $scope.limit,
+        skip: 0
+    };
+
+    getOrder();
+
+    function getOrder() {
+      Order.getList(requestParams).then(function(responses) {
+        $scope.orders = responses;
+        // update page numbers
+        if (responses && responses.pagination) {
+          $scope.totalItems = responses.pagination.total;
+        }
+      }).catch(function() {
+        $scope.orders = [];
+      });
+    };
+
   });
