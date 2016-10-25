28609f7dea718f8c152a9d9fcdbf5655e0008c58
11xi <xichen022@gmail.com>
2016-10-24 19:48:01 -0700

    Integrate with order that contains item object

--- client/app/scripts/controllers/orders.js
+++ client/app/scripts/controllers/orders.js
@@ -13,6 +13,28 @@ angular.module('clientApp').controller('OrdersCtrl', function($scope, Order) {
 
     getOrder();
 
+    $scope.hasImage = function(order) {
+      return order.item._gallery.order && order.item._gallery.order.length > 0;
+    };
+
+    $scope.getOrderImage = function(order) {
+      if(order && order.item) {
+        var gid = order.item._gallery._id;
+        return g_config.galleryUrl + '/' + gid + '/' + order.item._gallery.order[0];
+      }
+    };
+
+    // Page changed fn
+    $scope.pageChanged = function() {
+      // Set pagination params
+      requestParams.skip = getItemOffset();
+      getOrder();
+    };
+
+    function getItemOffset() {
+      return ($scope.currentPage - 1) * $scope.limit;
+    };
+    
     function getOrder() {
       Order.getList(requestParams).then(function(responses) {
         $scope.orders = responses;
