e87ebd6802bceda7b98cd03c2d064b92cbfded1b
Huaxi Wang <huaxi.wang@gmail.com>
2016-11-20 14:24:41 -0800

    Review manage placeholder.

---
+++ client/app/scripts/controllers/review-manage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var g_scope;
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:ReviewAddCtrl
+ * @description
+ * # ReviewAddCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('ReviewManageCtrl', function ($scope, Item, Review, $routeParams, $window,
+    AuthService, GalleryService, Restangular, itemUtil) {
+
+  });
